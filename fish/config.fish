3793b7663c18ad7522ae480cc319afc13fe2d274
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-03-19 18:35:14 -0500

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -135,3 +135,10 @@ set -g fish_pager_color_selected_background --background=$selection
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# pnpm
+set -gx PNPM_HOME "/home/mh/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
