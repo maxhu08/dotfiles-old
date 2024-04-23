3c7866bedb2cf6ea0acb5717210b7d76a5c791af
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-23 15:07:53 -0500

    update awesome rc

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -240,7 +240,7 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-  awful.button({}, 3, function() mymainmenu:toggle() end),
+-- awful.button({}, 3, function() mymainmenu:toggle() end), -- disable right click menu
   awful.button({}, 4, awful.tag.viewnext),
   awful.button({}, 5, awful.tag.viewprev)
 ))
