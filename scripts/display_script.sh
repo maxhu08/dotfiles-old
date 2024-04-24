c23295bbd4444589aea38d298f681981f451d483
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-23 21:07:19 -0500

    update display_script

--- scripts/display_script.sh
+++ scripts/display_script.sh
@@ -2,5 +2,9 @@
 
 # goes in ~/Dev/scripts/display_script.sh
 
-xrandr --output DP-2 --mode 1920x1080 --pos 0x0 --rate 165 --primary
+feh --bg-scale ~/Pictures/wallpapers/planet_ocean_wall.jpg
+
+xrandr --output DP-2 --mode 1920x1080 --pos 0x0 --rate 165 --primary 
 xrandr --output DP-1 --mode 1920x1080 --pos 1920x0 --rate 165
+
+feh --bg-scale ~/Pictures/wallpapers/planet_ocean_wall.jpg
